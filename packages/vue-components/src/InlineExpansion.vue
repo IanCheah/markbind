fd3533530279d36248d2d807b3950af2724c9f11
IanCheah <iancheah24@gmail.com>
2025-01-20 01:39:28 +0800

    Update default font of the hidden texts

--- packages/vue-components/src/InlineExpansion.vue
+++ packages/vue-components/src/InlineExpansion.vue
@@ -6,7 +6,7 @@
       v-on:click="showInlineText = !showInlineText">
       {{ header }}
     </button>
-    <div v-else>
+    <div v-else :style="computedStyle">
       <slot></slot>
     </div>
   </div>
@@ -20,22 +20,33 @@ export default  {
     header: {
       type: String,
       default: 'Header',
-    }
+    },
+    fontFamily: {
+      type: String,
+    },
   },
   data() {
     return {
       showInlineText: false,
     };
-  }
+  },
+  computed: {
+    computedStyle() {
+      return {
+        '--font-family': this.fontFamily || 'Impact'
+      };
+    },
+  },
 }
 </script>
 
 <style>
+
 .inline-panel {
   border: 1px solid #000;
   padding: 4px 6px;
   border-radius: 3px;
-  color: #f56;
+  color: #000;
   margin: 0 6px 4px 0;
   display: inline-block;
   width: auto;
